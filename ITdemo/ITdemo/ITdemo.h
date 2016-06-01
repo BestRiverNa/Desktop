436f2234dfe35a34a82724d983bc1dbdfb8fb5c1
BestRiverNa <student@BestRiven.local>
2016-06-01 09:46:38 +0800

    fed
    fhfgh

---
+++ ITdemo/ITdemo/ITdemo.h
@@ -0,0 +1,15 @@
+//
+//  ITdemo.h
+//  ITdemo
+//
+//  Created by student on 16/5/31.
+//  Copyright © 2016年 BestRiven. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface ITdemo : NSObject
+-(int)sum:(int)a :(int)b;
++(void)printLog;
+- (UIView *)customView;
+@end
